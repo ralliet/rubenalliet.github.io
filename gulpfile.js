3d4cb900e7fef1b72648d151d80241781ca795bc
Ruben Alliet <allietruben@gmail.com>
2018-04-02 19:44:29 +0200

    gulp file JS minify & CSS minify

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,29 @@
 const gulp = require('gulp');
-var gutil = require('gulp-util');
+const gutil = require('gulp-util');
+const rename = require("gulp-rename");
+const cleanCSS = require('gulp-clean-css');
+const uglify = require('gulp-uglify');
+const pump = require('pump');
 
-
-gulp.task('log', function() {
+gulp.task('log', function () {
     gutil.log('== My Log Task ==')
+});
+
+//minify css files
+gulp.task('minify-css', () => {
+    return gulp
+        .src('css/creative.css')
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest("css/"));
+});
+
+//minify js files
+gulp.task('minify-js', function (cb) {
+    pump([
+        gulp.src('js/creative.js'),
+        uglify(),
+        rename({suffix: '.min'}),
+        gulp.dest('js/')
+    ], cb);
 });
